576c181d79c7923bd2006ffc9dbe2f954a6c52ed
Gosha Bagrov <89145061+GoshaAB29@users.noreply.github.com>
2023-04-07 00:07:39 +0300

    Update solution.py

--- tf-data-analysis-stat-task2/solution.py
+++ tf-data-analysis-stat-task2/solution.py
@@ -7,9 +7,10 @@ from scipy.stats import norm
 chat_id = 578001199
 
 def solution(p: float, x: np.array) -> tuple:
-
-    alpha = 1 - p
-    loc = 2*x.mean() - 0.074
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 1024), \
-           loc - scale * norm.ppf(alpha / 1024)
+    a = 0.074
+    n = len(x)
+    x_max = x.max()
+    alpha = (1 - p)**(1./n)
+    left = x_max
+    right= (x_max - a) / alpha + a
+    return (left, right)
